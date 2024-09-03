96d589c607f60ca87e736beae990ed17ae9096b2
Jan Vícha <j.vicha@quadient.com>
2024-09-03 10:22:27 +0200

    Added Hello World sample

---
+++ hello-world/hello-world.ts
@@ -0,0 +1,11 @@
+export function getDescription() {
+    return {
+        description: "Hello World",
+        input: [],
+        output: []
+    } as const satisfies ScriptDescription;
+}
+
+export async function execute(context: Context): Promise<Output> {
+    console.log(`Hello World from ${context.environment.productionType} production!`);
+}
